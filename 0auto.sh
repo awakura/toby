c5b4d503729f3bd3d2850a54f6606ad916c9f4a7
awakura <51857132+awakura@users.noreply.github.com>
2024-04-07 10:28:13 +0900

    automated

--- 0auto.sh
+++ 0auto.sh
@@ -5,10 +5,21 @@
 #
 #pandoc -s -o index.html README.md
 #file --mime <filename>
-
+#iconv -f -t    change charset
 # md はutf-8 がデフォルト
 
-ls
+#md に<head>入れていいか？
 
+
+for f in ./*.md; do
+
+    pandoc -s -o tmp.html $f
+    sed 's/.md/.md.html/g' tmp.html > $f.html
+  # ファイル一つ毎の処理
+  echo "file: $f"
+ 
+done
+
+mv README.md.html index.md.html
 
 
